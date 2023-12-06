edf5cb49274c135d5166614bc2662fa9d7da7562
Creeper-boop <78140643+Creeper-boop@users.noreply.github.com>
2023-12-06 21:01:04 +0100

    new alloy release added alloy examples

---
+++ alloy/examples/blasterMG/script.lua
@@ -0,0 +1,39 @@
+BLASTER_MG = {
+  initMethod = function (self)
+    METH.initGenericWeapon(self)
+  end,
+	updateMethod = function(self, actor, time)
+		self:updateRecoil(actor, time)
+	end,
+	useMethod = function(self, actor, angle)
+		actor:emitSound(self:getUseSound(), 1, 1, self:getSoundRange())
+
+		local rec = self:getRecoilAmount()
+
+		actor:setStealthPenalty(self:getSoundRange() * SoundToVisFactor + rec * RecoilToVisFactor)
+
+		local x, y = actor:getSafeWeaponXY()
+		local obj = actor:shootProjectedBullet(x, y, angle, self.bulletSpeed, self.bullet, self)
+
+		self:createMuzzleFx(actor, x, y, "blasterMuzzle", 1, angle)
+		self:applyRandomRecoil(actor, rec)
+		self:applyRecoilFatigue()
+		self:useAmmo(actor, 1)
+
+		return true
+	end,
+  customSpriteNames = {
+    back = "plasmaAccelerator_back",
+    light = "plasmaAccelerator_light",
+    mag = "blastgun_magazine2"
+  },
+  renderMethod = function (self, x, y, angle, r, g, b, facingRight, glow)
+  	local cusprites = self.def.customSprites
+
+	  video.renderSpriteState(cusprites.back, x, y, 1, angle, 255, r, g, b, not facingRight)
+	  video.renderSpriteState(self.def.sprite, x, y, 1, angle, 255, r, g, b, not facingRight)
+	  video.renderSpriteState(cusprites.light, x, y, 1, angle, 255, r, g, b, not facingRight)
+	  video.renderSpriteState(cusprites.mag, x, y, 1, angle, 255, r, g, b, not facingRight)
+  end
+}
+print("ALLOY -- Hello from the test and example blaster alloy! :D")
